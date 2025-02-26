04f46c1f0cf0a23a7dca1c839d40c81857c4adfb
BrunoSantosOliveira27 <bruunoosanntos@outlook.com>
2025-02-26 20:25:48 -0300

    Finalizando injeção de dependencia na HomeController

--- GStore/Models/Produto.cs
+++ GStore/Models/Produto.cs
@@ -25,7 +25,7 @@ public class Produto
 
     [Required]
     [Range(0, int.MaxValue)]
-    public int Qtde { get; set; } = 0;
+    public int QtdeEstoque { get; set; } = 0;
 
     [Range(0, double.MaxValue)]
     [Column(TypeName = "numeric(10,2)")]
@@ -34,4 +34,8 @@ public class Produto
     [Range(0, double.MaxValue)]
     [Column(TypeName = "numeric(10,2)")]
     public decimal ValorVenda { get; set; }
+
+    public bool Destaque { get; set; } = false;
+
+    public List<ProdutoFoto> Fotos  { get; set; }
 }
